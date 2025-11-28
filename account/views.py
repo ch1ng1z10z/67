590c0750e8b6caef14e7eee46d8c9fd13ac79c69
ch1ng1z10z <ggckam228@main.ru>
2025-11-28 16:33:52 +0600

    low

---
+++ account/views.py
@@ -0,0 +1,21 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import login
+from .serializers import RegisterSerializer, LoginSerializer
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({"message": "User registered", "email": user.email})
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return Response({"message": "Logged in", "email": user.email})
